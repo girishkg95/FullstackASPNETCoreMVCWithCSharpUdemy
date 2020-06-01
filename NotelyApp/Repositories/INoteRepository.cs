63c0ea9a6d2fb80e0b06118760a88e0ff8fc40fa
girishkg95 <34678175+girishkg95@users.noreply.github.com>
2020-06-01 20:42:42 +0530

    NoteRepository has been created and the same is added as a service to the application as Singleton service inside the Configure  method of the Startup.cs file

---
+++ NotelyApp/Repositories/INoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using NotelyApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace NotelyApp.Repositories
+{
+    public interface INoteRepository
+    {
+        void deleteNote(NoteModel noteModel);
+        NoteModel FindNoteById(Guid id);
+        IEnumerable<NoteModel> GetAllNotes();
+        void SaveNote(NoteModel noteModel);
+    }
+}
